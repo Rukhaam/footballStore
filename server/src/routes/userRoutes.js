94b83dca98fd32cc4f9be37d3f090654c138b316
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-04-10 22:45:31 +0530

    rlm

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -4,12 +4,13 @@ import { getUserProfile, updateUserProfile, checkEmailExists } from '../controll
 import { requireAuth } from '../middlewares/authmiddleware.js';
 import { validateDTO } from '../middlewares/validateMiddleware.js';
 import { updateUserProfileDTO } from '../dtos/schemas.js';
+import { apiLimiter } from '../middlewares/rateLimiter.js';
 
 const router = express.Router();
 
+router.get('/check-email/:email', apiLimiter, checkEmailExists);
 
-router.get('/check-email/:email', checkEmailExists);
-
+// PROTECTED ROUTES
 router.use(requireAuth);
 router.get('/profile', getUserProfile);
 router.put('/profile', validateDTO(updateUserProfileDTO), updateUserProfile);
