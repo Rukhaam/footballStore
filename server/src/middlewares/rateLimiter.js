17ddeaf8dec7b735cdd62e0cd02febd1f42ff22c
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-04-10 22:39:01 +0530

    rlm

--- server/src/middlewares/rateLimiter.js
+++ server/src/middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, 
-  max: 100, 
+  max: 5, 
   message: { 
     error: 'Too many requests from this IP, please try again after 15 minutes.' 
   },
@@ -13,7 +13,7 @@ export const apiLimiter = rateLimit({
 
 export const checkoutLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
-  max: 10,
+  max: 5,
   message: { 
     error: 'Too many checkout attempts. Please try again later.' 
   },
